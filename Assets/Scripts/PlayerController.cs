f711e1af2f1c343daf63bf4c189e8beff6562fcf
Nick <103617694@student.swin.edu.au>
2026-09-17 12:10:32 +1000

    Interaction Panel
    Added Interaction Panel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField]
 	private float moveSpeed = 5f;
+
 	[SerializeField]
 	private float sprintMultiplier = 2f;
 
+	[SerializeField]
+	private float interactionRadius = 1.5f;
+
 	private Rigidbody2D rb;
+
 	private InputAction moveAction;
 	private InputAction sprint;
 	private InputAction interact;
 
 	private Vector2 moveInput;
 	private bool isSprinting;
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
@@ -25,24 +30,88 @@ public class PlayerController : MonoBehaviour
 		sprint = InputSystem.actions.FindAction("Sprint");
 		interact = InputSystem.actions.FindAction("Interact");
 
-		interact.performed += Interact;
+		Debug.Log("Move Action: " + moveAction);
+		Debug.Log("Sprint Action: " + sprint);
+		Debug.Log("Interact Action: " + interact);
+
+		if (moveAction != null)
+			moveAction.Enable();
+
+		if (sprint != null)
+			sprint.Enable();
+
+		if (interact != null)
+			interact.Enable();
 	}
 
 	void Update()
 	{
-		moveInput = moveAction.ReadValue<Vector2>();
-		isSprinting = sprint.ReadValue<float>() > 0.5f;
+		// Movement
+		if (moveAction != null)
+		{
+			moveInput = moveAction.ReadValue<Vector2>();
+		}
+
+		// Sprint
+		if (sprint != null)
+		{
+			isSprinting = sprint.ReadValue<float>() > 0.5f;
+		}
+
+		// Interaction
+		if (interact != null && interact.WasPressedThisFrame())
+		{
+			Debug.Log("E WAS PRESSED!");
+
+			TryInteract();
+		}
 	}
 
 	void FixedUpdate()
 	{
-		float currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
+		if (rb == null)
+			return;
+
+		float currentSpeed =
+			isSprinting
+				? moveSpeed * sprintMultiplier
+				: moveSpeed;
+
 		rb.linearVelocity = moveInput * currentSpeed;
 	}
 
-	void Interact(CallbackContext context)
+	private void TryInteract()
 	{
-		// Added this incase we want to use it in the future
-		// Interact was one of the default actions in the input system
+		Collider2D[] objects =
+			Physics2D.OverlapCircleAll(
+				transform.position,
+				interactionRadius
+			);
+
+		Debug.Log("Checking for nearby assets...");
+
+		foreach (Collider2D obj in objects)
+		{
+			AssetInteraction asset =
+				obj.GetComponent<AssetInteraction>();
+
+			if (asset != null)
+			{
+				Debug.Log("ASSET FOUND: " + asset.assetName);
+
+				asset.SelectAsset();
+				return;
+			}
+		}
+
+		Debug.Log("NO ASSET NEARBY!");
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireSphere(
+			transform.position,
+			interactionRadius
+		);
 	}
 }
