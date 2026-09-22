1667c9167d2492a42e0340ecd28e5cbc2c78c101
Judah Walsh <judahw58@gmail.com>
2026-09-22 18:48:09 +1000

    Added Character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,6 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -10,39 +10,38 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	private float sprintMultiplier = 2f;
 
+	private static readonly int XDirHash = Animator.StringToHash("XDir");
+	private static readonly int YDirHash = Animator.StringToHash("YDir");
+	private Animator animator;
 	private Rigidbody2D rb;
 	private InputAction moveAction;
 	private InputAction sprint;
-	private InputAction interact;
 
 	private Vector2 moveInput;
 	private bool isSprinting;
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		TryGetComponent(out animator);
 
 		moveAction = InputSystem.actions.FindAction("Move");
 		sprint = InputSystem.actions.FindAction("Sprint");
-		interact = InputSystem.actions.FindAction("Interact");
-
-		interact.performed += Interact;
 	}
 
 	void Update()
 	{
 		moveInput = moveAction.ReadValue<Vector2>();
 		isSprinting = sprint.ReadValue<float>() > 0.5f;
+		if (animator != null)
+		{
+			animator.SetInteger(XDirHash, (int)Math.Round(moveInput.x));
+			animator.SetInteger(YDirHash, (int)Math.Round(moveInput.y));
+		}
 	}
 
 	void FixedUpdate()
 	{
 		float currentSpeed = isSprinting ? moveSpeed * sprintMultiplier : moveSpeed;
 		rb.linearVelocity = moveInput * currentSpeed;
-	}
-
-	void Interact(CallbackContext context)
-	{
-		// Added this incase we want to use it in the future
-		// Interact was one of the default actions in the input system
 	}
 }
