8ad6d3149ffd810c983a9ebf894b7856fb7348ca
Darcy368 <105920569@student.swin.edu.au>
2026-09-22 15:35:43 +1000

    added a range in which player can click object

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -4,6 +4,9 @@ using UnityEngine.EventSystems;
 
 public class Object : MonoBehaviour, IPointerClickHandler
 {
+    public GameObject player;
+    
+    
     //types determine whether the object is or isn't publicly managed
     public types myType = new types();
     //chosenTypes determine what the player has selected the object as
@@ -15,5 +18,22 @@ public class Object : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)//happens when object is clicked
     {
         Debug.Log(this.name + " has been clicked");
+        
+        if (playerwithinRange(7))
+        {
+            Debug.Log(this.name + " has been clicked and is within range");
+        }
+    }
+
+    private bool playerwithinRange(int range) //checks player is within selection range
+    {
+        Vector3 playerv = player.transform.position;
+        float distance = Vector2.Distance(playerv, this.transform.position);
+        if (distance < range)
+        {
+            return (true);
+        }else{
+            return(false);
+        }
     }
 }
