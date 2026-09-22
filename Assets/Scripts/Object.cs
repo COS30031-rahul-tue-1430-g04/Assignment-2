1c57f17c7ba831c54cac518779591231a0afbbb4
Darcy368 <105920569@student.swin.edu.au>
2026-09-22 10:10:11 +1000

    added types for the object (privately or publicly managed)

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -4,11 +4,15 @@ using UnityEngine.EventSystems;
 
 public class Object : MonoBehaviour, IPointerClickHandler
 {
+    //types determine whether the object is or isn't publicly managed
     public types myType = new types();
+    //chosenTypes determine what the player has selected the object as
+    public chosenTypes myChosenType = new chosenTypes();
     
     public enum types{publiclyManaged, privatelyManaged};
+    public enum chosenTypes{notChosen, publiclyManaged, privatelyManaged};
     
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)//happens when object is clicked
     {
         Debug.Log(this.name + " has been clicked");
     }
